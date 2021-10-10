ee185e101af3ccb2b2513e4f7cf315628d3ec38f
Gideon Crawley <gscrawley@gmail.com>
2021-10-09 20:00:10 -0700

    made HomeScreen and SettingScreen components

--- App.js
+++ App.js
@@ -3,21 +3,8 @@ import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import  HomeScreen  from './components/HomeScreen';
+import  SettingScreen  from './components/SettingScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -25,7 +12,7 @@ export default function App() {
   return (<NavigationContainer>
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Settings" component={SettingScreen} />
     </Tab.Navigator>
   </NavigationContainer>
 );
@@ -34,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },

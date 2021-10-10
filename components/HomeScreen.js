43117d53e0067c9f2560d4bb3f158753ea2d6684
Gideon Crawley <gscrawley@gmail.com>
2021-10-09 20:23:16 -0700

    styled home and settings pages

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -3,9 +3,24 @@ import { Text, View, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+      <View style={styles.container }>
+        <Text style={styles.title}>Welcome Home!</Text>
       </View>
     );
   }
   
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        width:'100%', 
+        height: '90%', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        backgroundColor:'lightgreen'
+    },
+    title: {
+        fontSize: 36, 
+        fontWeight: 'bold', 
+        color: 'orange'
+    }
+})
--- components/SettingScreen.js
+++ components/SettingScreen.js
@@ -3,8 +3,24 @@ import { Text, View, StyleSheet } from 'react-native';
 
 export default function SettingsScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Settings!</Text>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        width:'100%', 
+        height: '90%', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        backgroundColor:'orange'
+    },
+    title: {
+        fontSize: 36, 
+        fontWeight: 'bold', 
+        color: 'lightgreen'
+    }
+})

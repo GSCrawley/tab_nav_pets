80f2f8dc50b8bee6a13c6bbe4a7f6fd12f0bbe33
Gideon Crawley <gscrawley@gmail.com>
2021-10-10 00:50:39 -0700

    set icons for navigation between cat and dog screens

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, FontAwesome5 } from 'react-native-vector-icons'
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import { FontAwesome5 } from '@expo/vector-icons'; 
 
 import  HomeScreen  from './components/HomeScreen';
 import  SettingScreen  from './components/SettingScreen';
@@ -9,8 +12,26 @@ import  SettingScreen  from './components/SettingScreen';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (<NavigationContainer>
-    <Tab.Navigator>
+  return (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused 
+              ? 'cat'
+              : 'cat'
+          } else if (route.name === 'Settings') {
+            iconName = focused 
+            ? 'dog' : 'dog'
+          }
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+      })}
+>
+
+
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingScreen} />
     </Tab.Navigator>

2d4661c11a55e76dc2cdff8a76b84576cd034cb0
Gideon Crawley <gscrawley@gmail.com>
2021-10-11 00:10:17 -0700

    Dog Screen w/Dog List and Cat Sceen w/ Cat List ready to go

---
+++ components/ListCats.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FlatList, View } from 'react-native'
+import Cell from './Cell'
+import { cats } from './breeds'
+
+export default function ListCats({navigation}) {
+  return (
+  <FlatList
+      style={{ flex:1, padding: 110, paddingTop: 20, marginBottom:40, backgroundColor:'lightblue' }}
+      data={cats}
+      renderItem= {( {item, index} ) => {
+        return (
+          <Cell 
+            title={` ${item.breed} `} 
+            data={item}
+            key={index.breed} 
+            showDetails={() => navigation.navigate('Details', { item })} 
+          />
+        )
+      }}
+
+      keyExtractor={(item) => item.breed}
+    />
+    
+  )
+}
---
+++ components/ListDogs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FlatList } from 'react-native'
+import Cell from './Cell'
+import { dogs } from './breeds'
+
+export default function ListDogs({navigation}) {
+  return (
+    <FlatList
+      style={{ flex:1, padding: 110, paddingTop: 20, marginBottom:40, backgroundColor:'lightblue' }}
+      data= {dogs}
+      renderItem= {( {item, index} ) => {
+        return (
+          <Cell 
+            title={` ${item.breed} `} 
+            data={item} 
+            key={index.breed}
+            showDetails={() => navigation.navigate('Details', { item })} 
+          />
+        )
+      }}
+      keyExtractor={(item) => item.breed}
+    />
+  )
+}
